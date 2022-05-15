61b4e2837650500eb05559cdaef98fa64ac7642e
MarcioCV <marciocaldasvieira@outlook.com>
2022-05-15 01:45:15 -0300

    mobile nav fixed

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const btn_mobile = document.getElementById("btn_mobile");
 
 function toggle_Menu() {
-    const nav = document.getElementById("nav");
+    const nav = document.getElementById("div_mobile_nav");
     nav.classList.toggle('active');
 }
 btn_mobile.addEventListener('click',toggle_Menu);
