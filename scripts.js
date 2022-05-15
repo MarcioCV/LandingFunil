07c7b6d41a64b08dbb384ab873220ade9c94335b
Danizinh <danimaltoc@gmai.com>
2022-05-14 23:56:58 -0300

    script

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const btn_mobile = document.getElementById("btn_mobile");
+
+function toggle_Menu() {
+    const nav = document.getElementById("nav");
+    nav.classList.toggle('active');
+}
+btn_mobile.addEventListener('click'.toggle_Menu);
