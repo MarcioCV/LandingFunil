71dcd8abe6df7832e81ed993932efdee38f2f225
MarcioCV <marciocaldasvieira@outlook.com>
2022-05-31 14:35:54 -0300

    video review 90% done

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
 const btnMobile = document.getElementById('btn-mobile');
+const card = document.getElementsByClassName('card_content');
+const btnClose = document.getElementById('close_video');
+var video = document.getElementById('video_review');
 
 function toggleMenu(event) {
   if (event.type === 'touchstart') event.preventDefault();
@@ -10,6 +13,17 @@ function toggleMenu(event) {
     event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
   } else {
     event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+
+function toggleVideo(event) {
+  console.log(video.style.display)
+  if(video.style.display == "none"){
+    video.style.display = "flex";
+  }
+  else{
+    video.style.display = "none";
   }
 }
 
