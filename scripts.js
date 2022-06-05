9aa004a51f8e3907f397d491c7a264f174bc89c4
MarcioCV <marciocaldasvieira@outlook.com>
2022-06-05 19:02:53 -0300

    story review done

--- scripts.js
+++ scripts.js
@@ -1,12 +1,8 @@
 const btnMobile = document.getElementById('btn-mobile');
 const card = document.getElementsByClassName('card_content');
-const btnClose = document.getElementById('close_video');
-var video = document.getElementById('video_review');
+var videos = document.querySelectorAll('.div_review_video');
 const button_open = document.querySelectorAll('.button_story')
-const button_close = document.getElementById('close_video')
-console.log(button_open)
-
-
+const button_close = document.querySelectorAll('.close_video')
 
 function toggleMenu(event) {
   if (event.type === 'touchstart') event.preventDefault();
@@ -21,33 +17,39 @@ function toggleMenu(event) {
   }
 }
 
-
-async function toggleVideo(event){
-
-  if(video.style.display != "flex"){
-    console.log("rodou, setado pra flex")
-    video.style.display = "flex";
+function toggleVideo(i){
+  if(videos[i].style.display != "flex"){
+    videos[i].style.display = "flex";
     setTimeout(() =>{
-      video.classList.toggle('fade')
+      videos[i].classList.toggle('fade')
     }, 100);
   }
   else{
-    console.log("entrou no else")
-    video.classList.toggle('fade')
+    videos[i].classList.toggle('fade')
     setTimeout(() =>{
-      console.log("rodou, setado pra none")
-      video.style.display = "none";
+      videos[i].style.display = "none";
     }, 300);
   }
 }
 
 
 
-button_open[0].addEventListener('click', toggleVideo)
-button_open[1].addEventListener('click', toggleVideo)
-button_open[2].addEventListener('click', toggleVideo)
-button_open[3].addEventListener('click', toggleVideo)
-button_open[4].addEventListener('click', toggleVideo)
-button_close.addEventListener('click', toggleVideo)
+button_open[0].addEventListener('click',function(){toggleVideo(0)},false)
+button_open[1].addEventListener('click',function(){ toggleVideo(1)},false)
+button_open[2].addEventListener('click',function(){ toggleVideo(2)},false)
+button_open[3].addEventListener('click',function(){ toggleVideo(3)},false)
+button_open[4].addEventListener('click',function(){ toggleVideo(4)},false)
+button_open[5].addEventListener('click',function(){ toggleVideo(5)},false)
+button_open[6].addEventListener('click',function(){ toggleVideo(6)},false)
+button_open[7].addEventListener('click',function(){ toggleVideo(7)},false)
+
+button_close[0].addEventListener('click',function(){toggleVideo(0)},false)
+button_close[1].addEventListener('click',function(){ toggleVideo(1)},false)
+button_close[2].addEventListener('click',function(){ toggleVideo(2)},false)
+button_close[3].addEventListener('click',function(){ toggleVideo(3)},false)
+button_close[4].addEventListener('click',function(){ toggleVideo(4)},false)
+button_close[5].addEventListener('click',function(){ toggleVideo(5)},false)
+button_close[6].addEventListener('click',function(){ toggleVideo(6)},false)
+button_close[7].addEventListener('click',function(){ toggleVideo(7)},false)
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
