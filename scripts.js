c97c6426329cd2c29d7aace3905df3688f9708c9
Danizinh <danimaltoc@gmai.com>
2022-06-09 23:16:42 -0300

    in progress of changes

--- scripts.js
+++ scripts.js
@@ -17,39 +17,82 @@ function toggleMenu(event) {
   }
 }
 
-function toggleVideo(i){
-  if(videos[i].style.display != "flex"){
+function toggleVideo(i) {
+  if (videos[i].style.display != "flex") {
     videos[i].style.display = "flex";
-    setTimeout(() =>{
+    setTimeout(() => {
       videos[i].classList.toggle('fade')
     }, 100);
   }
-  else{
+  else {
     videos[i].classList.toggle('fade')
-    setTimeout(() =>{
+    setTimeout(() => {
       videos[i].style.display = "none";
     }, 300);
   }
 }
 
 
+button_open[0].addEventListener('click', function () { toggleVideo(0) }, false)
+button_open[1].addEventListener('click', function () { toggleVideo(1) }, false)
+button_open[2].addEventListener('click', function () { toggleVideo(2) }, false)
+button_open[3].addEventListener('click', function () { toggleVideo(3) }, false)
+button_open[4].addEventListener('click', function () { toggleVideo(4) }, false)
+button_open[5].addEventListener('click', function () { toggleVideo(5) }, false)
+button_open[6].addEventListener('click', function () { toggleVideo(6) }, false)
+button_open[7].addEventListener('click', function () { toggleVideo(7) }, false)
 
-button_open[0].addEventListener('click',function(){toggleVideo(0)},false)
-button_open[1].addEventListener('click',function(){ toggleVideo(1)},false)
-button_open[2].addEventListener('click',function(){ toggleVideo(2)},false)
-button_open[3].addEventListener('click',function(){ toggleVideo(3)},false)
-button_open[4].addEventListener('click',function(){ toggleVideo(4)},false)
-button_open[5].addEventListener('click',function(){ toggleVideo(5)},false)
-button_open[6].addEventListener('click',function(){ toggleVideo(6)},false)
-button_open[7].addEventListener('click',function(){ toggleVideo(7)},false)
-
-button_close[0].addEventListener('click',function(){toggleVideo(0)},false)
-button_close[1].addEventListener('click',function(){ toggleVideo(1)},false)
-button_close[2].addEventListener('click',function(){ toggleVideo(2)},false)
-button_close[3].addEventListener('click',function(){ toggleVideo(3)},false)
-button_close[4].addEventListener('click',function(){ toggleVideo(4)},false)
-button_close[5].addEventListener('click',function(){ toggleVideo(5)},false)
-button_close[6].addEventListener('click',function(){ toggleVideo(6)},false)
-button_close[7].addEventListener('click',function(){ toggleVideo(7)},false)
+button_close[0].addEventListener('click', function () { toggleVideo(0) }, false)
+button_close[1].addEventListener('click', function () { toggleVideo(1) }, false)
+button_close[2].addEventListener('click', function () { toggleVideo(2) }, false)
+button_close[3].addEventListener('click', function () { toggleVideo(3) }, false)
+button_close[4].addEventListener('click', function () { toggleVideo(4) }, false)
+button_close[5].addEventListener('click', function () { toggleVideo(5) }, false)
+button_close[6].addEventListener('click', function () { toggleVideo(6) }, false)
+button_close[7].addEventListener('click', function () { toggleVideo(7) }, false)
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
+
+
+function myFunction() {
+  console.log(document.documentElement.clientWidth)
+  var element = document.getElementsByClassName("carousel-desktop");
+  var element2 = document.getElementsByClassName("carousel-mobile");
+
+  if (document.documentElement.clientWidth < 887) {
+    console.log("Removeu desktop add mobile")
+    element[0].classList.remove("carousel-item");
+    element[1].classList.remove("carousel-item");
+    element[0].classList.remove("active");
+    element[1].classList.remove("active");
+
+    element2[0].classList.add("carousel-item")
+    element2[0].classList.add("active")
+    element2[1].classList.add("carousel-item")
+    element2[2].classList.add("carousel-item")
+    element2[3].classList.add("carousel-item")
+  }
+  else {
+    if (element[0].classList.contains("carousel-item") == false){
+      
+      element[0].classList.add("carousel-item")
+      element[1].classList.add("carousel-item")
+      element[0].classList.add("active")
+
+      element2[0].classList.remove("active")
+      element2[1].classList.remove("active")
+      element2[2].classList.remove("active")
+      element2[3].classList.remove("active")
+
+      element2[0].classList.remove("carousel-item")
+      element2[1].classList.remove("carousel-item")
+      element2[2].classList.remove("carousel-item")
+      element2[3].classList.remove("carousel-item")
+    }
+  }
+}
+
+
+window.addEventListener("resize", myFunction);
+
+window.onload(myFunction())
